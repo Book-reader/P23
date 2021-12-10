236cc1dc5d97ba7abbeaba2ad7788674c7790665
Book-reader <89329785+Book-reader@users.noreply.github.com>
2021-12-10 18:38:55 +1300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,24 +23,19 @@ function setup() {
   world = engine.world;
   angleMode(DEGREES);
 
-  playerBase = new PlayerBase(300,500,180,150);
-  player = new Player(285,playerBase.body.position.y-153,50,180);
-
- 
-
-  playerArcher = new PlayerArcher(
-    340,
-    playerBase.body.position.y - 180,
-    120,
-    120
-  );
-
-  board1 = new Board(width - 300, 330, 50, 200);
-  board2 = new Board(width - 550, height - 300, 50, 200);
+  var options = { isStatic: true }; 
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options); 
+  World.add(world, playerBase); 
+  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options); 
+  World.add(world,player) 
+  playerArcher = new PlayerArcher( 340, playerBase.position.y - 112, 120, 120 ); 
+  arrow = new PlayerArrow( playerArcher.body.position.x, playerArcher.body.position.y, 100, 10 );
 }
 
 function draw() {
   background(backgroundImg);
+  arrow.display();
+  playerArcher.display();
 
   Engine.update(engine);
   image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
@@ -52,10 +47,13 @@ function draw() {
   textSize(40);
   text("EPIC ARCHERY", width / 2, 100);
 
+  if (keyCode === 32){
+    arrow.shoot(playerArcher.body.angle);
+  }
   
 }
 
-function display() {
+/*function display() {
   var pos = this.body.position;
   var angle = this.body.angle;
 
@@ -65,6 +63,6 @@ function display() {
   imageMode (CENTER);
   image(this.image,0,0,this.width,this.height);
   pop ();
-}
+}*/
 
 
